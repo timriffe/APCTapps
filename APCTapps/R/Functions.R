b8e6789f02103db76163c27bc964341febf33e59
Tim Riffe <tim.riffe@gmail.com>
2017-04-24 19:16:35 +0200

    graph creation for poster

--- APCTapps/R/Functions.R
+++ APCTapps/R/Functions.R
@@ -372,3 +372,125 @@ n_triads <- function(n){
 
 #n_triads(5)
 
+#'
+#' @title LexRefN Overlays semi-transparent N-year lexis triangle outlines onto a Lexis plot
+#' 
+#' @description This is an optional overlay function for Lexis map plots.
+#' 
+#' @param ages vector of ages, e.g. \code{0:110}. These are y values.
+#' @param years vector of years, e.g. \code{1900:2010}. These are x values.
+#' @param N. age interval. Default of 5.
+#' @param Chrono logical. Default \code{TRUE}. Birth cohorts? If \code{FALSE} it assumes we have a TPD diagram and gives death cohorts.
+#' @param equilateral logical. Default \code{FALSE}. Do we want 60 degree or 90/45 degree angles?
+#' @param ... optional arguments passed to \code{segments()}
+#' 
+#' @export
+#' 
+
+LexRefN <- function(ages, years, N = 5, chrono = TRUE, equilateral = FALSE,col1=gray(.5),col2=gray(.5),col3=gray(.5),...){
+	# vertical
+	#par(mai=c(.5, .5, .5, .5), xaxs = "i", yaxs = "i")
+	#plot(NULL, xlim = c(1820,2060), ylim = c(0,100), axes = FALSE, ylab = "", xlab = "", asp = 1)
+	#LexRefN(0:100, 1900:2000, col = "#A5A5A5", N = 20, xpd=TRUE)
+	# ages<-0:100; years <- 1900:2000; col = "#A5A5A5"; N = 20; equilateral <- TRUE; Chrono = TRUE
+	# Chrono = FALSE
+	minA      <- min(ages)
+	maxA      <- max(ages)
+	yearshift <- ifelse(equilateral, .5, 0) * ifelse(chrono, 1, -1)
+	agemult   <- ifelse(equilateral, sqrt(3) / 2, 1)
+	
+	
+	segments(years[years %% N == 0] - minA * yearshift,
+			min(ages) * agemult,
+			years[years %% N == 0] - maxA * yearshift,
+			max(ages) * agemult,
+			col = col1,
+			...
+	)
+	
+	# horizontal lines
+	segments(min(years) - ages[ages %% N == 0] * yearshift,
+			ages[ages %% N == 0] * agemult,
+			max(years) - ages[ages %% N == 0] * yearshift,
+			ages[ages %% N == 0] * agemult,
+			col = col2,
+			...
+	)
+	# diag cohort references, bit more tricky:
+	l.years <- (min(years) - ages) 
+	coh.ext <- range(c(l.years[l.years %% N == 0], years[years %% N == 0]))
+	cohs    <- seq(coh.ext[1], coh.ext[2], by = N)
+	
+	# bottom, left:
+	xl  <- cohs + min(ages)
+	yb <- rep(min(ages), length(cohs))
+	yb[xl < min(years)] <- yb[xl < min(years)] + min(years) - xl[xl < min(years)]
+	xl[xl < min(years)] <- min(years)
+	
+	# top, right:
+	xr  <- cohs + max(ages)
+	yt  <- rep(max(ages), length(cohs))
+	yt[xr > max(years)] <- yt[xr > max(years)] - xr[xr > max(years)] + max(years)
+	xr[xr > max(years)] <- max(years)
+	
+	# cut down one last time:
+	xr <- xr[yt >= min(ages)]
+	xl <- xl[yt >= min(ages)]
+	yb <- yb[yt >= min(ages)]
+	yt <- yt[yt >= min(ages)]
+	
+	# draw cohort refs:
+	if (chrono){
+		segments(xl - yb * yearshift, 
+				yb * agemult, 
+				xr - yt * yearshift, 
+				yt * agemult, 
+				col = col3,
+				...
+		)
+	} else {
+		segments(rev(xr)- yb * yearshift, 
+				yb * agemult, 
+				rev(xl)- yt * yearshift, 
+				yt * agemult,
+				col = col3,
+				...
+		)
+	}
+	
+}
+
+ATLRefN <- function(ages, N = 5, equilateral = FALSE, col1="#A5A5A5",col2="#A5A5A5",col3="#A5A5A5",...){
+	
+	minA      <- min(ages)
+	maxA      <- max(ages)
+	Th <- C   <- sort(unique(ages[as.integer(ages) %% N == 0]))
+	
+	if (equilateral){
+		# left-leaning
+		segments(C,rep(minA, length(C)) * sqrt(3)/2 ,C*.5, Th * sqrt(3)/2, col = col1,...)
+		
+		# right-leaning
+		segments(C,rep(minA, length(C)) * sqrt(3)/2, C + rev(Th)*.5,rev(Th) * sqrt(3)/2, col = col2, ...)
+		
+		# horizontals.
+		segments(C - .5 * Th, Th * sqrt(3) / 2, rev(C) + .5 * Th, Th * sqrt(3) / 2, col = col3, ...)
+		
+	} else {
+		# verticals
+		xv  <- rev(C)
+		y1v <- rep(minA,length(C))
+		y2v <- Th
+		segments(xv,y1v,xv,y2v, col = col1, ...)
+		
+		# horizontals
+		x1h <- rep(minA, length(Th))
+		x2h <- rev(C)
+		yh  <- Th
+		segments(x1h,yh,x2h,yh, col = col2, ...)
+		
+		# diagonals
+		segments(rep(minA,length(Th)),Th,C,rep(minA,length(C)), col = col3, ...)
+	}
+}
+
--- APCTapps/R/baseballPoster.R
+++ APCTapps/R/baseballPoster.R
@@ -6,6 +6,7 @@ library(lubridate)
 library(reshape2)
 library(RColorBrewer)
 library(dplyr)
+library(fields)
 source("/home/tim/git/APCTapps/APCTapps/R/Functions.R")
 
 
@@ -69,7 +70,17 @@ Dat 	 <- Dat[Dat$d7 <= 101 | is.na(Dat$d7), ]
 ramp <- colorRampPalette(RColorBrewer::brewer.pal(9,"YlGn"),space="Lab")
 
 #make function to take first 2 elements of each triad, plot BA average
-SurfGeneric <- function(Dat, x="d1",y="p1",stat = "BA", agg = mean,contour=FALSE,labels,ramp,smoothit=TRUE,...){
+SurfGeneric <- function(Dat, triad.i,stat = "BA", agg = mean,contour=FALSE,labels,ramp,smoothit=TRUE,i,plot=TRUE,pars=FALSE...){
+	
+	anyp <- any(grepl("p",triad.i))
+	if (anyp){
+		x <- rev(sort(triad.i[grepl("p",triad.i)]))[1]
+		y <- triad.i[grepl("d",triad.i)]
+	} else {
+		x <- triad.i[1]
+		y <- triad.i[3]
+	}
+	
 	Dati     <- Dat
 	
 	keep     <- !is.na(Dati[,x]) & !is.na(Dati[,y])
@@ -79,21 +90,30 @@ SurfGeneric <- function(Dat, x="d1",y="p1",stat = "BA", agg = mean,contour=FALSE
 	#any(is.na(Dati[,y]))
 	#denom   <- acast(Dat, as.formula(paste0(y,"~",x)),length,value.var="yearID",fill = NA_real_)
 	
-	Surf    <- acast(Dati, as.formula(paste0(y,"~",x)),agg,value.var=stat,fill=NA_real_,na.rm=TRUE)
-	xat     <- as.integer(colnames(Surf))
-	yat     <- as.integer(rownames(Surf))
+	Surf     <- acast(Dati, as.formula(paste0(y,"~",x)),agg,value.var=stat,fill=NA_real_,na.rm=TRUE,drop=FALSE)
+	xat      <- as.integer(colnames(Surf))
+	yat      <- as.integer(rownames(Surf))
 	if (smoothit){
-	Surfsm <- image.smooth(Surf)$z
+	Surfsm   <- image.smooth(Surf)$z
 	Surfsm[is.na(Surf)] <- NA
-	Surf <- Surfsm
+	Surf     <- Surfsm
     }
-	
+	colnames(Surf) <- xat
+	rownames(Surf) <- yat
 	Nplayers <- length(unique(Dati$playerID))
 	Nobs     <- nrow(Dati)
+	if (plot){
 	image(xat,yat,t(Surf),asp=1, xlab = labels[x], ylab = labels[y], 
-			main = paste(as.character(substitute(agg)),stat,labels[y],"by",labels[x]),
+			main = paste(as.character(substitute(agg)),stat,labels[y],
+					"by",labels[x],"\ntriad nr",i,"\n(",labels[triad.i[2]],"in diag)"),
 			sub = paste0(Nobs, " observations of ", Nplayers, " players"),col=ramp)
 	if(contour)	contour(xat,yat,t(Surf),add=TRUE)
+    }
+	out <- list(Surf=Surf,xat=xat,yat=yat,Nobs=Nobs,Nplayers=Nplayers,x=x,y=y,triad=triad.i)
+	if (pars){
+		return(invisible(out))
+	}
+	
 }
 
 # BA: battive avg
@@ -107,54 +127,60 @@ cv <- function(x,...){sd(x,...)/mean(x,...)}
 
 stats <- c("BA","PA","TB","SlugPct","OBP","OPS","BABIP")
 
+labels[triads[[12]]]
+labels[triads[[14]]]
+labels[triads[[16]]]
+
+labels[triads[[12]]]
+labels[triads[[14]]]
+labels[triads[[16]]]
+
+
 # sd
 lapply(stats, function(stat, Dat){
-			path <- paste0("/home/tim/git/APCTapps/APCTapps/Figures/triads",stat,"sd.pdf")
+			path <- paste0("/home/tim/git/APCTapps/APCTapps/Figures/BaseballTriads/triads",stat,"sd.pdf")
 			pdf(path)
 			for (i in 1:length(triads)){
 				SurfGeneric(Dat=Dat, 
-						x = triads[[i]][1], 
-						y = triads[[i]][2], 
+						triad.i = triads[[i]], 
 						stat = stat, 
 						agg = cv, 
 						labels = labels, 
 						na.rm=TRUE,
 						ramp=rev(ramp(20)),
-						contour=TRUE)
+						contour=TRUE,i=i)
 			}
 			dev.off()
 		}, Dat=Dat)
 # mean
 lapply(stats, function(stat, Dat){
-			path <- paste0("/home/tim/git/APCTapps/APCTapps/Figures/triads",stat,"mean.pdf")
+			path <- paste0("/home/tim/git/APCTapps/APCTapps/Figures/BaseballTriads/triads",stat,"mean.pdf")
 			pdf(path)
 			for (i in 1:length(triads)){
 				SurfGeneric(Dat=Dat, 
-						x = triads[[i]][1], 
-						y = triads[[i]][2], 
+						triad.i = triads[[i]], 
 						stat = stat, 
 						agg = mean, 
 						labels = labels, 
 						na.rm=TRUE,
 						ramp=rev(ramp(20)),
-						contour=TRUE)
+						contour=TRUE,i=i)
 			}
 			dev.off()
 		}, Dat=Dat)
 # CV
 lapply(stats, function(stat, Dat){
-			path <- paste0("/home/tim/git/APCTapps/APCTapps/Figures/triads",stat,"CV.pdf")
+			path <- paste0("/home/tim/git/APCTapps/APCTapps/Figures/BaseballTriads/triads",stat,"CV.pdf")
 			pdf(path)
 			for (i in 1:length(triads)){
 				SurfGeneric(Dat=Dat, 
-						x = triads[[i]][1], 
-						y = triads[[i]][2], 
+						triad.i = triads[[i]], 
 						stat = stat, 
 						agg = cv, 
 						labels = labels, 
 						na.rm=TRUE,
 						ramp=rev(ramp(20)),
-						contour=TRUE)
+						contour=TRUE,i=i)
 			}
 			dev.off()
 		}, Dat=Dat)
@@ -164,5 +190,279 @@ lapply(stats, function(stat, Dat){
 # Monday TODO:
 # make grid of surfaces, more artisanally
 
+labels[triads[[12]]]
+labels[triads[[14]]]
+labels[triads[[16]]]
+
+
+# CV
+labels[triads[[12]]]
+
+CVBA1 <- SurfGeneric(Dat=Dat, triad.i = triads[[12]], 
+		stat = "BA", agg = cv, labels = labels, 
+		ramp=rev(ramp(20)),contour=TRUE,i=12,pars=TRUE)
+ATLRefN(0:50)
+Surf <- CVBA1$Surf
+NAout <- row(Surf)-col(Surf)[,ncol(Surf):1] > 0
+Surf[NAout] <- NA
+breaks <- pretty(Surf,n=20)
+pdf("/home/tim/git/APCTapps/APCTapps/Figures/CVBA1.pdf",width=6,height=6)
+par(xaxs="i",yaxs="i",xpd=FALSE)
+image(CVBA1$xat+.5,CVBA1$yat+.5,t(Surf),asp=1, 
+		xlab = labels[CVBA1$x], ylab = labels[CVBA1$y],
+		col=rev(ramp(length(breaks)-1)),breaks=breaks, 
+		ylim= c(0,30), xlim=c(15,45), axes = FALSE,
+		panel.first=list(
+				polygon(x=c(15,45,15),y=c(0,0,30), border=NA,col=gray(.85)),
+				ATLRefN(0:45,col1="white",col2="white",col3="white"),
+				text(seq(15,45,5),0,seq(15,45,5),pos=1,xpd=TRUE),
+				text(15,seq(0,30,5),seq(0,30,5),pos=2,xpd=TRUE)
+))
+contour(CVBA1$xat,CVBA1$yat,t(Surf),add=TRUE)
+ATLRefN(0:45,col1="#FFFFFF40",col2="#FFFFFF40",col3="#FFFFFF40")
+dev.off()
+
+# limit age retired to 35
+
+
+
+CVBA2 <- SurfGeneric(Dat=Dat, triad.i = triads[[14]], 
+		stat = "BA", agg = cv, labels = labels, 
+		ramp=rev(ramp(20)),contour=TRUE,i=14,plot = FALSE,pars=TRUE)
+Surf <- CVBA2$Surf
+NAout <- row(Surf)-col(Surf)[,ncol(Surf):1] > -5
+Surf[NAout] <- NA
+breaks <- pretty(Surf,n=20)
+pdf("/home/tim/git/APCTapps/APCTapps/Figures/CVBA2.pdf",width=6,height=6)
+par(xaxs="i",yaxs="i",xpd=FALSE)
+image(CVBA2$xat+.5,CVBA2$yat+.5,t(Surf),asp=1, 
+		xlab = labels[CVBA2$x], ylab = labels[CVBA2$y],
+		col=rev(ramp(length(breaks)-1)),breaks=breaks, 
+		ylim= c(0,35), xlim=c(15,50), axes = FALSE,
+		panel.first=list(
+				polygon(x=c(15,50,15),y=c(0,0,35), border=NA,col=gray(.85)),
+				ATLRefN(0:50,col1="white",col2="white",col3="white",xpd=TRUE),
+				text(seq(15,50,5),0,seq(15,50,5),pos=1,xpd=TRUE),
+				text(15,seq(0,35,5),seq(0,35,5),pos=2,xpd=TRUE)
+		))
+contour(CVBA2$xat,CVBA2$yat,t(Surf),add=TRUE)
+ATLRefN(0:50,col1="#FFFFFF40",col2="#FFFFFF40",col3="#FFFFFF40")
+dev.off()
+# limit age retired to 35
+
+CVBA3 <- SurfGeneric(Dat=Dat, triad.i = triads[[16]], 
+		stat = "BA", agg = cv, labels = labels, 
+		ramp=rev(ramp(20)),contour=TRUE,i=16,plot = TRUE,pars=TRUE)
+# limit career length to 33
+Surf <- CVBA3$Surf
+NAout <- row(Surf)-col(Surf)[,ncol(Surf):1] > -2
+Surf[NAout] <- NA
+breaks <- pretty(Surf,n=20)
+pdf("/home/tim/git/APCTapps/APCTapps/Figures/CVBA3.pdf",width=6,height=6)
+par(xaxs="i",yaxs="i",xpd=FALSE)
+image(CVBA3$xat+1.5,CVBA3$yat+.5,t(Surf),asp=1, 
+		xlab = labels[CVBA3$x], ylab = labels[CVBA3$y],
+		col=rev(ramp(length(breaks)-1)),breaks=breaks, 
+		ylim= c(0,30), xlim=c(0,30), axes = FALSE,
+		panel.first=list(
+				polygon(x=c(0,30,0),y=c(0,0,30), border=NA,col=gray(.85)),
+				ATLRefN(0:30,col1="white",col2="white",col3="white",xpd=TRUE),
+				text(seq(0,30,5),0,seq(0,30,5),pos=1,xpd=TRUE),
+				text(0,seq(0,30,5),seq(0,30,5),pos=2,xpd=TRUE)
+		))
+contour(CVBA3$xat+1.5,CVBA3$yat,t(Surf),add=TRUE)
+ATLRefN(0:30,col1="#FFFFFF40",col2="#FFFFFF40",col3="#FFFFFF40")
+dev.off()
+
+
+
+
+
+
+meanBA1 <- SurfGeneric(Dat=Dat, triad.i = triads[[12]], 
+		stat = "BA", agg = mean, labels = labels, 
+		ramp=rev(ramp(20)),contour=TRUE,i=12,plot = FALSE,pars=TRUE)
+# limit age retired to 35
+Surf <- meanBA1$Surf
+NAout <- row(Surf)-col(Surf)[,ncol(Surf):1] > 0
+Surf[NAout] <- NA
+breaks <- pretty(Surf,n=20)
+pdf("/home/tim/git/APCTapps/APCTapps/Figures/meanBA1.pdf",width=6,height=6)
+par(xaxs="i",yaxs="i",xpd=FALSE)
+image(meanBA1$xat+.5,meanBA1$yat+.5,t(Surf),asp=1, 
+		xlab = labels[meanBA1$x], ylab = labels[meanBA1$y],
+		col=ramp(length(breaks)-1),breaks=breaks, 
+		ylim= c(0,30), xlim=c(15,45), axes = FALSE,
+		panel.first=list(
+				polygon(x=c(15,45,15),y=c(0,0,30), border=NA,col=gray(.85)),
+				ATLRefN(0:45,col1="white",col2="white",col3="white"),
+				text(seq(15,45,5),0,seq(15,45,5),pos=1,xpd=TRUE),
+				text(15,seq(0,30,5),seq(0,30,5),pos=2,xpd=TRUE)
+		))
+contour(meanBA1$xat,meanBA1$yat,t(Surf),add=TRUE)
+ATLRefN(0:45,col1="#FFFFFF40",col2="#FFFFFF40",col3="#FFFFFF40")
+dev.off()
+
+meanBA2 <- SurfGeneric(Dat=Dat, triad.i = triads[[14]], 
+		stat = "BA", agg = mean, labels = labels, 
+		ramp=rev(ramp(20)),contour=TRUE,i=14,plot = FALSE,pars=TRUE)
+# limit age retired to 35
+Surf <- meanBA2$Surf
+NAout <- row(Surf)-col(Surf)[,ncol(Surf):1] > -5
+Surf[NAout] <- NA
+breaks <- pretty(Surf,n=20)
+pdf("/home/tim/git/APCTapps/APCTapps/Figures/meanBA2.pdf",width=6,height=6)
+par(xaxs="i",yaxs="i",xpd=FALSE)
+image(meanBA2$xat+.5,meanBA2$yat+.5,t(Surf),asp=1, 
+		xlab = labels[meanBA2$x], ylab = labels[meanBA2$y],
+		col=ramp(length(breaks)-1),breaks=breaks, 
+		ylim= c(0,35), xlim=c(15,50), axes = FALSE,
+		panel.first=list(
+				polygon(x=c(15,50,15),y=c(0,0,35), border=NA,col=gray(.85)),
+				ATLRefN(0:50,col1="white",col2="white",col3="white",xpd=TRUE),
+				text(seq(15,50,5),0,seq(15,50,5),pos=1,xpd=TRUE),
+				text(15,seq(0,35,5),seq(0,35,5),pos=2,xpd=TRUE)
+		))
+contour(meanBA2$xat+.5,meanBA2$yat+.5,t(Surf),add=TRUE)
+ATLRefN(0:50,col1="#FFFFFF40",col2="#FFFFFF40",col3="#FFFFFF40")
+dev.off()
+
+
+meanBA3 <- SurfGeneric(Dat=Dat, triad.i = triads[[16]], 
+		stat = "BA", agg = mean, labels = labels, 
+		ramp=ramp(20),contour=TRUE,i=16,plot = FALSE,pars=TRUE)
+# limit career length to 33 or 32
+Surf <- meanBA3$Surf
+NAout <- row(Surf)-col(Surf)[,ncol(Surf):1] > -3
+Surf[NAout] <- NA
+breaks <- pretty(Surf,n=20)
+pdf("/home/tim/git/APCTapps/APCTapps/Figures/meanBA3.pdf",width=6,height=6)
+par(xaxs="i",yaxs="i",xpd=FALSE)
+image(meanBA3$xat+1.5,meanBA3$yat+.5,t(Surf),asp=1, 
+		xlab = labels[meanBA3$x], ylab = labels[meanBA3$y],
+		col=ramp(length(breaks)-1),breaks=breaks, 
+		ylim= c(0,30), xlim=c(0,30), axes = FALSE,
+		panel.first=list(
+				polygon(x=c(0,30,0),y=c(0,0,30), border=NA,col=gray(.85)),
+				ATLRefN(0:30,col1="white",col2="white",col3="white",xpd=TRUE),
+				text(seq(0,30,5),0,seq(0,30,5),pos=1,xpd=TRUE),
+				text(0,seq(0,30,5),seq(0,30,5),pos=2,xpd=TRUE)
+		))
+contour(meanBA3$xat+1.5,meanBA3$yat,t(Surf),add=TRUE)
+ATLRefN(0:30,col1="#FFFFFF40",col2="#FFFFFF40",col3="#FFFFFF40")
+dev.off()
+
+
+
+
+
+labels[triads[[2]]]
+# BA long surfs
+CVBAp1 <- SurfGeneric(Dat=Dat, triad.i = triads[[2]], 
+		stat = "BA", agg = cv, labels = labels, 
+		ramp=rev(ramp(20)),contour=TRUE,i=2,plot = FALSE,pars=TRUE)
+LexRefN(ages=15:50,years=1870:2015)
+Surf <- CVBAp1$Surf
+breaks <- pretty(Surf,n=20)
+pdf("/home/tim/git/APCTapps/APCTapps/Figures/CVBAp1.pdf",width=19,height=6)
+par(xaxs="i",yaxs="i",xpd=FALSE)
+image(CVBAp1$xat+.5,CVBAp1$yat+.5,t(Surf),asp=1, 
+		xlab = labels[CVBAp1$x], ylab = labels[CVBAp1$y],
+		col=rev(ramp(length(breaks)-1)),breaks=breaks, 
+		xlim= c(1870,2016), ylim=c(15,50), axes = FALSE,
+		panel.first=list(
+				rect(1870,15,2016,50, border=NA,col=gray(.85)),
+				LexRefN(ages=15:50,years=1870:2015,col1="white",col2="white",col3="white",xpd=TRUE),
+				text(seq(1870,2010,10),15,seq(1870,2010,10),pos=1,xpd=TRUE),
+				text(1870,seq(15,50,5),seq(15,50,5),pos=2,xpd=TRUE)
+		))
+contour(CVBAp1$xat+.5,CVBAp1$yat+.5,t(Surf),add=TRUE)
+ATLRefN(0:30,col1="#FFFFFF40",col2="#FFFFFF40",col3="#FFFFFF40")
+LexRefN(ages=15:50,years=1870:2015,col1="#FFFFFF40",col2="#FFFFFF40",col3="#FFFFFF40",xpd=TRUE)
+dev.off()
+
+
+# y lim 15-50 APC
+# OK
+labels[triads[[3]]]
+CVBAp2 <- SurfGeneric(Dat=Dat, triad.i = triads[[3]], 
+		stat = "BA", agg = cv, labels = labels, 
+		ramp=rev(ramp(20)),contour=TRUE,i=3,plot = TRUE,pars=TRUE)
+Surf <- CVBAp2$Surf
+a <- 16:50;y<- 1871:2015
+Surf2 <- matrix(NA,nrow=length(a),ncol=length(y),dimnames=list(a,y))
+ind <- as.integer(rownames(Surf)) < 51
+Surf2[rownames(Surf[ind,]),colnames(Surf[ind,])] <- Surf[ind,]
+Surf <- Surf2
+CVBAp2$yat <- as.integer(rownames(Surf))
+breaks <- pretty(Surf,n=20)
+pdf("/home/tim/git/APCTapps/APCTapps/Figures/CVBAp2.pdf",width=19,height=6)
+par(xaxs="i",yaxs="i",xpd=FALSE)
+image(CVBAp2$xat+.5,CVBAp2$yat+.5,t(Surf),asp=1, 
+		xlab = labels[CVBAp2$x], ylab = labels[CVBAp2$y],
+		col=rev(ramp(length(breaks)-1)),breaks=breaks, 
+		xlim= c(1870,2016), ylim=c(15,50), axes = FALSE,
+		panel.first=list(
+				rect(1870,15,2016,50, border=NA,col=gray(.85)),
+				LexRefN(ages=15:50,years=1870:2015,col1="white",col2="white",col3="white",xpd=TRUE),
+				text(seq(1870,2010,10),15,seq(1870,2010,10),pos=1,xpd=TRUE),
+				text(1870,seq(15,50,5),seq(15,50,5),pos=2,xpd=TRUE)
+		))
+contour(CVBAp2$xat+.5,CVBAp2$yat+.5,t(Surf),add=TRUE)
+LexRefN(ages=15:50,years=1870:2015,col1="#FFFFFF40",col2="#FFFFFF40",col3="#FFFFFF40",xpd=TRUE)
+dev.off()
+# y lim 15-50 (age retired in y). Maybe take birth coh after 1870?
+
+# no need to cut these
+labels[triads[[5]]]
+CVBAp3 <- SurfGeneric(Dat=Dat, triad.i = triads[[5]], 
+		stat = "TB", agg = cv, labels = labels, 
+		ramp=rev(ramp(20)),contour=TRUE,i=5,pars=TRUE)
+Surf <- CVBAp3$Surf
+breaks <- pretty(Surf,n=20)
+pdf("/home/tim/git/APCTapps/APCTapps/Figures/CVBAp3.pdf",width=19,height=6)
+par(xaxs="i",yaxs="i",xpd=FALSE)
+image(CVBAp3$xat+.5,CVBAp3$yat+1.5,t(Surf),asp=1, 
+		xlab = labels[CVBAp3$x], ylab = labels[CVBAp3$y],
+		col=rev(ramp(length(breaks)-1)),breaks=breaks, 
+		xlim= c(1870,2016), ylim=c(0,30), axes = FALSE,
+		panel.first=list(
+				rect(1870,0,2016,30, border=NA,col=gray(.85)),
+				LexRefN(ages=0:30,years=1870:2015,col1="white",col2="white",col3="white",xpd=TRUE),
+				text(seq(1870,2010,10),0,seq(1870,2010,10),pos=1,xpd=TRUE),
+				text(1870,seq(0,30,5),seq(0,30,5),pos=2,xpd=TRUE)
+		))
+contour(CVBAp3$xat+.5,CVBAp3$yat+1.5,t(Surf),add=TRUE)
+LexRefN(ages=0:30,years=1870:2015,col1="#FFFFFF40",col2="#FFFFFF40",col3="#FFFFFF40",xpd=TRUE)
+dev.off()
+
+
+
+
+labels[triads[[8]]]
+CVBAp4 <- SurfGeneric(Dat=Dat, 
+		triad.i = triads[[8]], 
+		stat = "TB", 
+		agg = cv, 
+		labels = labels, 
+		ramp=rev(ramp(20)),
+		contour=TRUE,i=8,pars=TRUE)
+Surf <- CVBAp4$Surf
+breaks <- pretty(Surf,n=20)
+pdf("/home/tim/git/APCTapps/APCTapps/Figures/CVBAp4.pdf",width=19,height=6)
+par(xaxs="i",yaxs="i",xpd=FALSE)
+image(CVBAp4$xat+.5,CVBAp4$yat+1.5,t(Surf),asp=1, 
+		xlab = labels[CVBAp4$x], ylab = labels[CVBAp4$y],
+		col=rev(ramp(length(breaks)-1)),breaks=breaks, 
+		xlim= c(1870,2016), ylim=c(0,30), axes = FALSE,
+		panel.first=list(
+				rect(1870,0,2016,30, border=NA,col=gray(.85)),
+				LexRefN(ages=0:30,years=1870:2015,col1="white",col2="white",col3="white",xpd=TRUE,chrono=FALSE),
+				text(seq(1870,2010,10),0,seq(1870,2010,10),pos=1,xpd=TRUE),
+				text(1870,seq(0,30,5),seq(0,30,5),pos=2,xpd=TRUE)
+		))
+contour(CVBAp4$xat+.5,CVBAp4$yat+1.5,t(Surf),add=TRUE)
+LexRefN(ages=0:30,years=1870:2015,col1="#FFFFFF40",col2="#FFFFFF40",col3="#FFFFFF40",xpd=TRUE,chrono=FALSE)
+dev.off()
 
 
